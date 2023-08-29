e298dd2ab849c3d81e67acdd225d112f78755bdf
Ontiveros Alfaro Johán Alejandro <111895521+ChemsJam@users.noreply.github.com>
2023-08-29 11:22:22 -0600

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  constructor(private router: Router){}
+  login(){
+    this.router.navigate(['\home']);
+  }
+  
 }
